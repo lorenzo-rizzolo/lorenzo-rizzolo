2900920fd5dca35ce19cc8ef7b3b762fdb5bbfcd
lorenzo-rizzolo <lorenzo.rizzolo@istitutoagnelli.it>
2023-01-14 20:13:04 +0100

    start project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function show_pop(){
+    console.log("pop-up")
+    let pop = document.getElementById('pop')
+    pop.style.display = "block"
+}
+function close_pop(){
+    console.log("pop-up")
+    let pop = document.getElementById('pop')
+    pop.style.display = "none"
+}
